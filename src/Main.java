921c43eefd3832505ee3658d29eca7b959ab55f3
khalouiyesser <111162300+khalouiyesser@users.noreply.github.com>
2023-10-02 23:26:06 +0100

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Animal lion = new Animal("animals","Grand",15,true);
+        Zoo myZoo = new Zoo("Belvidère", "Tunis", 25);
+
+        myZoo.animals = new Animal[]{lion};
+
+        System.out.println("Informations sur l'animal :");
+        System.out.println("Famille : " + lion.family);
+        System.out.println("Nom : " + lion.name);
+        System.out.println("Âge : " + lion.age + " ans");
+        System.out.println("Est un mammifère : " + lion.isMammal);
+
+        System.out.println("\nInformations sur le zoo :");
+        System.out.println("Nom du zoo : " + myZoo.name);
+        System.out.println("Ville : " + myZoo.city);
+        System.out.println("Nombre de cages : " + myZoo.nbrCages);
+        System.out.println("Animaux dans le zoo : " + myZoo.animals.length);
+
     }
 }
--- src/Zoo.java
+++ src/Zoo.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Zoo {
+public class Zoo {
+
+    Animal[] animals;
+    String name;
+    String city;
+    int nbrCages;
+
+    public Zoo(String name, String city, int nbrCages){
+
+        this.name= name;
+        this.city= city;
+        this.nbrCages= nbrCages;
+    }
 }
